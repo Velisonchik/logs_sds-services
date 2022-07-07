8758b46fd30dd2c756adc0a1714edf0d7d355b1b
velisonchik <velisonchik@gmail.com>
2022-07-07 16:44:08 +0300

    Улучшил текст оповещания

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ def scan_files(pdir):
                         if ("error" in l.lower() or "exception" in l.lower()) and check_lines(ident_file):
                             if f.count(l) > 1:
                                 open('exc.txt', 'a').write(ident_file)
-                                send_notification(f'Ошибка в файле {pdir}+\\+{pname}, строка {f.index(l) + 1}')
+                                send_notification(f'Ошибка в файле {pdir}+\\+{pname}.\nCтрока:{f.index(l) + 1}\nТекст:{l}')
                                 f[f.index(l)] = ''
                                 print(f)
                             else:
                                 open('exc.txt', 'a').write(ident_file)
-                                send_notification(f'Ошибка в файле {pdir}+\\+{pname}, строка {f.index(l) + 1}')
+                                send_notification(f'Ошибка в файле {pdir}+\\+{pname}.\nCтрока:{f.index(l) + 1}\nТекст:{l}')
                                 f[f.index(l)] = ''
                         else:
                             f[f.index(l)] = ''
