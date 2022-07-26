35c25160a254de2e5a6d86b83c356494d060509d
velisonchik <velisonchik@gmail.com>
2022-07-26 11:09:56 +0300

    Переход на watchdog

--- main.py
+++ main.py
@@ -1,21 +1,89 @@
 import os
 import sys
+import sqlite3
 import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 import requests
-import clear_exc
 import threading
 from bot_req import *
 
 excs = ["declare error_code", "ignore error code"]
 
 
+class OnMyWatch:
+    # Set the directory on watch
+
+    def __init__(self, path):
+        self.watchDirectory = path
+        self.observer = Observer()
+
+    def run(self):
+        print(f'running on %s' % self.watchDirectory)
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.watchDirectory, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(0.1)
+        except:
+            self.observer.stop()
+            print("Observer Stopped")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            # Event is created, you can process it now
+            scan_file(event.src_path)
+        elif event.event_type == 'modified':
+            # Event is modified, you can process it now
+            scan_file(event.src_path)
+
+
 def send_notification(msg_text):
     for gid in groups_id:
         try:
-            url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" + gid + "&parse_mode=HTML" + "&text=" + msg_text
-            results = requests.get(url_req)
+            url_req = "https://api.telegram.org/bot" + TOKEN + "/sendMessage" + "?chat_id=" + gid + "&parse_mode=HTML" \
+                      + "&text=" + msg_text
+            requests.get(url_req)
         except Exception as e:
             print(e)
+
+
+def sqlite_insert(temp_f, len_f):
+    try:
+        sqlite_connection = sqlite3.connect(dbname)
+        cursor = sqlite_connection.cursor()
+        cursor.execute(f"INSERT or replace INTO exc values(NULL,'{temp_f}',{len_f});")
+        sqlite_connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при insert к sqlite", error)
+
+
+def sqlite_select_lastline(temp_f):
+    try:
+        sqlite_connection = sqlite3.connect(dbname)
+        cursor = sqlite_connection.cursor()
+        cursor.execute(f"SELECT last_readline FROM exc WHERE file_name='{temp_f}'")
+        res = cursor.fetchone()
+        cursor.close()
+        if res is None:
+            return -1
+        else:
+            return res[0]
+
+    except sqlite3.Error as error:
+        print("Ошибка при select к sqlite", error)
 
 
 def is_exc(pl):
@@ -26,68 +94,71 @@ def is_exc(pl):
     return False
 
 
-def check_lines(test):
-    for line in open('exc.txt', 'r').readlines():
-        if test == line:
-            return False
-    return True
+def first_read(pname):
+    if sqlite_select_lastline(pname) == -1:
+        return True
+    else:
+        return False
 
 
-def scan_files(pdir):
+def scan_file(pname):
+    if pname.endswith('.txt') or pname.endswith('.log'):
+        with open(pname, 'r') as f:
+            f = f.readlines()
+            len_f = len(f)
+            if first_read(pname):
+                sqlite_insert(pname, len_f)
+            else:
+                fname = pname[pname.rfind('\\') + 1:]
+                dname = pname[:pname.rfind('\\')]
+                template_head = f'<b>{fname}</b> ({dname})\n\n'
+                msg = template_head
+                # start_index_file = sqlite_select_lastline(pname)
+                # f = f[start_index_file:]
+                for line in f[sqlite_select_lastline(pname):]:
+                    if ("error" in line.lower() or "exception" in line.lower()) and not is_exc(line.lower()):
+                        if len(msg + f'Cтрока: {f.index(line) + 1}\n{line}\n') > 4090:
+                            send_notification(msg)
+                            msg = template_head
+                        else:
+                            msg += f'Cтрока: {f.index(line) + 1}\n{line}\n'
+                            f[f.index(line)] = ''
+                    else:
+                        f[f.index(line)] = ''
+                sqlite_insert(pname, len_f)
+                if msg != template_head:
+                    send_notification(msg)
+
+
+def con_DB():
     try:
-        while True:
-            ident_file = ''
-            for pname in os.listdir(pdir):
-                if os.path.isfile(pdir + '\\' + pname):
-                    if pname.endswith('.txt') or pname.endswith('.log'):
-                        if os.path.getmtime(pdir + '\\' + pname) - time.time() >= -100:
-                            with open(pdir + '\\' + pname, 'r') as f:
-                                template_head = f'<b>{pname}</b> ({pdir})\n\n'
-                                msg = template_head
-                                f = f.readlines()
-                                for l in f:
-                                    if "error" in l.lower() or "exception" in l.lower():
-                                        ident_file = pdir + '\\' + pname + ' ' + l.replace('\n', '') + ' ' + str(
-                                            f.index(l) + 1) + '\n'
-                                        if check_lines(ident_file) and not is_exc(l.lower()):
-                                            open('exc.txt', 'a').write(ident_file)
-                                            if len(msg + f'Cтрока: {f.index(l) + 1}\n{l}\n') > 4090:
-                                                send_notification(msg)
-                                                msg = template_head
-                                            else:
-                                                msg += f'Cтрока: {f.index(l) + 1}\n{l}\n'
-                                                # send_notification(f'<b>{pname}</b> ({pdir})\nCтрока: {f.index(l) + 1}\n{l}')
-                                                f[f.index(l)] = ''
-                                    else:
-                                        f[f.index(l)] = ''
-                            if msg != template_head:
-                                send_notification(msg)
-                elif not os.path.isfile(pdir + '\\' + pname):
-                    scan_files(pdir + '\\' + pname)
-            break
-    except Exception as e:
-        print(e)
+        sqlite_connection = sqlite3.connect(dbname)
+        cursor = sqlite_connection.cursor()
+        print("База данных создана и успешно подключена к SQLite")
+        sqlite_select_query = "CREATE TABLE IF NOT EXISTS exc (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," \
+                              "file_name file_name TEXT NOT NULL, last_readline last_readline INTEGER NOT NULL,UNIQUE" \
+                              " (file_name));"
+        cursor.execute(sqlite_select_query)
+        cursor.close()
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
 
 
-def main(cell):
-    with open(f'exc.txt', 'a'):
-        pass
-    try:
-        while True:
-            file_stats = os.stat('exc.txt')
-            if file_stats.st_size / (1024 * 1024) > 2:
-                clear_exc.clear()
-            scan_files(cell)
-    except Exception as e:
-        print(e)
+def watchdog_run(path):
+    watch = OnMyWatch(path=path)
+    watch.run()
 
 
 if __name__ == '__main__':
     try:
         path_dirs = sys.argv[1].split(',')
+        if not os.path.exists(dbname):
+            with open(dbname, 'w') as _:
+                pass
+        con_DB()
     except IndexError:
         print('Не указана директория с которой необходимо работать.')
         exit(1)
     for path_dir in path_dirs:
-        dir_thread = threading.Thread(target=main, args=(path_dir,))
+        dir_thread = threading.Thread(target=watchdog_run, args=(path_dir,))
         dir_thread.start()
